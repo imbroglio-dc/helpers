ab4a7f07c586c35c10c6445e8da11d431d17c58c
David Chen <david.chen49@berkeley.edu>
2024-09-26 14:12:37 -0700

    fix

--- make_file_dirs.R
+++ make_file_dirs.R
@@ -6,17 +6,18 @@ make_file_dirs <- function(wd = NULL, data = TRUE, R = TRUE, output = TRUE,
   } else {
     wd <- getwd()
   }
-  if (data & !dir.exists(paste0(wd, "data/")))
-    dir.create(paste0(wd, "data/"))
-  if (R & !dir.exists(paste0(wd, "R/")))
-    dir.create(paste0(wd, "R/"))
-  if (output & !dir.exists(paste0(wd, "output/")))
-    dir.create(paste0(wd, "output/"))
-  if (docs & !dir.exists(paste0(wd, "docs/")))
-    dir.create(paste0(wd, "docs/"))
-  if (scratch & !dir.exists(paste0(wd, "scratch/"))){
-    dir.create(paste0(wd, "scratch/"))
-    if (!any(grepl("scratch", readLines(".gitignore"), ignore.case = TRUE)))
+  if (data & !dir.exists(paste0(wd, "/data/")))
+    dir.create(paste0(wd, "/data/"))
+  if (R & !dir.exists(paste0(wd, "/R/")))
+    dir.create(paste0(wd, "/R/"))
+  if (output & !dir.exists(paste0(wd, "/output/")))
+    dir.create(paste0(wd, "/output/"))
+  if (docs & !dir.exists(paste0(wd, "/docs/")))
+    dir.create(paste0(wd, "/docs/"))
+  if (scratch & !dir.exists(paste0(wd, "/scratch/"))){
+    dir.create(paste0(wd, "/scratch/"))
+    if (!file.exists(".gitignore") | 
+        !any(grepl("scratch", try(readLines(".gitignore")), ignore.case = TRUE)))
       write(x = "\n # Scratch folder \n scratch/ \n", file = ".gitignore", append = TRUE)
   }
 
